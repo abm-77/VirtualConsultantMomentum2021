510de6624719fc2ee585de2d39c7518e704bc96e
Bryson <andrewbrysonmiller@gmail.com>
2021-01-13 17:51:51 -0500

    Outlinining
    Outlined most of the pages necessary for the project MVP. We don't really have to worry about all the other pages for the project presentation.

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,8 +1,51 @@
 from app import app
 from flask import render_template
 
+
 # Home Page
 @app.route("/")
 def index():
     return render_template("public/index.html") 
+
+
+### STORE VIEWS ###
+# TODO: Implement Storefront
+@app.route("/store")
+def store ():
+    return "Store"
+
+@app.route("/store/module/<moduleID>")
+def module_store_page(moduleID):
+    return f"Module Store Page: {moduleID}"
+
+@app.route("/testmodule")
+def testmodule ():
+    return render_template("public/testmodule.html")
+
+
+### MODULE VIEWS ###
+@app.route("/modules/<moduleID>")
+def module_content_page(moduleID):
+    return f"Module Page: {moduleID}"
+
+
+### USER VIEWS ###
+# TODO: Implement Signup
+@app.route("/signup")
+def signup():
+    return "Signup"
+
+# TODO: Implement Login 
+@app.route("/login")
+def Login ():
+    return "Login"
+
+# TODO: Implement Profiles
+@app.route("/profile/<userID>")
+def profile (userID):
+    return "Profile"
+
+@app.route("/profile/<userID>/mymodules")
+def profile_modules (userID):
+    return "Profile Modules"
 
