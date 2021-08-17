af4a738cd92353c373cfb65fa233fe932d013d87
Andrew Miller <65512175+abm-77@users.noreply.github.com>
2021-08-16 20:41:06 -0400

    Update __init__.py

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 
 # Secret Key
-app.config["SECRET_KEY"] = b']\xdf\x8d\x03\x8f\xaf\xde\xa7\x89\x18\x83U\xce\xbf\xa6t'
-app.config["SECURITY_PASSWORD_SALT"] = "temporary"
+app.config["SECRET_KEY"] =
+app.config["SECURITY_PASSWORD_SALT"] = ""
 
 # Database
-client = pymongo.MongoClient("mongodb+srv://BeaverAdmin:zkzueLbNEWpEuo9j@cluster0.0ypwg.mongodb.net/project_luna?retryWrites=true&w=majority")
+client = pymongo.MongoClient("")
 database = client["project_luna"]
 
 from app import views
